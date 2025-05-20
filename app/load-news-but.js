1ce6679535f61a621445a32dadf58149ff67236b
Ali Hasanov <nan.fgbs@gmail.com>
2025-05-20 06:14:38 +0400

    📦 init: First Commit-2

--- app/load-news-but.js
+++ app/load-news-but.js
@@ -1,5 +1,3 @@
-//todo:========Buttons========//
-
 function handleClick(id) {
     console.log(`site - ${id}`);
     window.location.href = `./news.html?id=${id}`;
@@ -11,18 +9,28 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-function runNewsFunction(){
+function runNewsFunction() {
     const params = new URLSearchParams(window.location.search);
-    const id = params.get("id"); 
-    console.log(`ID from URL: ${id}`);
+    const id = params.get("id");
+
+    if (!id || isNaN(id)) {
+        console.error("Некорректный ID");
+        return;
+    }
 
     fetch('https://682aeb9eab2b5004cb383ffe.mockapi.io/news/news')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        document.getElementById("img-1").style.backgroundImage = `url("${data[id].avatar}")`;
-        document.querySelector(".mainText").textContent = `${data[id].data}`;
-        document.getElementById("news-name").textContent = `${data[id].name}`;
-    }
-)
+        .then(res => res.json())
+        .then(data => {
+            if (!data[id]) {
+                console.error(`Нет данных по id = ${id}`);
+                return;
+            }
+
+            document.getElementById("img-1").style.backgroundImage = `url("${data[id].avatar}")`;
+            document.querySelector(".mainText").textContent = data[id].data;
+            document.getElementById("news-name").textContent = data[id].name;
+        })
+        .catch(err => {
+            console.error("Ошибка при загрузке данных:", err);
+        });
 }
